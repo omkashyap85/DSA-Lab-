bbd5327322e03812ac6f2b5bcd4084bf0610d9d5
omkashyap85 <158665619+omkashyap85@users.noreply.github.com>
2024-03-30 14:52:59 +0530

    Create pro_30.c
     Preorder traversal of binary tree

---
+++ DSA_lab_program/pro_30.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int key;
+    struct node *left, *right;
+};
+
+struct node* createnode(int key){
+    struct node* newnode = (struct node*) malloc (sizeof(struct node));
+    newnode->key = key;
+    newnode->left = NULL;
+    newnode->right = NULL;
+    return newnode;
+}
+
+struct node* preordertraversal(struct node* root){
+    if(root==NULL){
+        return NULL;
+    }
+    printf("%d ",root->key);
+    preordertraversal(root->left);
+    preordertraversal(root->right);
+}
+
+struct node* createbinary(struct node* root){
+    int key;
+    printf("Enter the data: ");
+    scanf("%d ", &key);
+    if(key == -1){
+        return NULL;
+    }
+    root = createnode(key);
+    printf("Enter data in left of %d", key);
+    root->left = createbinary(root->left);
+    printf("Enter data in right of %d", key);
+    root->right = createbinary(root->right);
+    return root;
+}
+
+int main(){
+    struct node* root =NULL;
+    root = createbinary(root);
+    return 0;
+}
