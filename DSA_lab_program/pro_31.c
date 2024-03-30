9873531de78f74e492c3452638801417e5e1605c
omkashyap85 <158665619+omkashyap85@users.noreply.github.com>
2024-03-30 14:54:20 +0530

    Create pro_31.c
    Post order traversal

---
+++ DSA_lab_program/pro_31.c
@@ -0,0 +1,46 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int key;
+    struct node *left, *right;
+};
+
+struct node* createnode(int key){
+    struct node* newnode = (struct node*) malloc (sizeof(struct node));
+    newnode->key = key;
+    newnode->left = NULL;
+    newnode->right = NULL;
+    return newnode;
+}
+
+void postorder(struct node* root){
+    if(root==NULL){
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ", root->key);
+}
+
+struct node* createbinary(struct node* root){
+    int key;
+    printf("Enter the data: ");
+    scanf("%d ", &key);
+    if(key == -1){
+        return NULL;
+    }
+    root = createnode(key);
+    printf("Enter data in left of %d", key);
+    root->left = createbinary(root->left);
+    printf("Enter data in right of %d", key);
+    root->right = createbinary(root->right);
+    return root;
+}
+
+int main(){
+    struct node* root =NULL;
+    root = createbinary(root);
+    return 0;
+}
