26953c576fec14959722ea44115f3f5f843508f6
omkashyap85 <158665619+omkashyap85@users.noreply.github.com>
2024-03-30 14:27:10 +0530

    Update pro_16.c
     Link list insertion and deletion at beginning

--- DSA_lab_program/pro_16.c
+++ DSA_lab_program/pro_16.c
@@ -1,33 +1,39 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct node{
+struct node {
     int key;
     struct node* next;
 };
 
 struct node* head;
 
-void createnode(int key){
+struct node* createnode(int key) {
     struct node *newnode = (struct node*) malloc(sizeof(struct node));
     newnode->key = key;
     newnode->next = NULL;
+    return newnode;
 }
 
-void insertatbegning(int key){
+void insertatbegning(int key) {
     struct node* temp = createnode(key);
     temp->next = head;
     head = temp;
 }
 
-void deleteatbegning(){
+void deleteatbegning() {
+    if (head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
     struct node* temp = head;
     head = temp->next;
     free(temp); 
 }
 
-int main(){
-    struct node* head = NULL;
-    head->key = 1;
+int main() {
+    head = NULL;
+    head = createnode(1);
     head->next = createnode(2);
+    return 0;
 }
